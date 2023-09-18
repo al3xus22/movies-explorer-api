3eca55275ee51fda9130fe75e42a226ca43007da
Aleksey Oleynik <al3xus22@yandex.ru>
2023-09-18 22:16:46 +0300

    fix: отключил время токена

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes');
 const serverError = require('./errors/server-error');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 mongoose.connect(DB_URL);
 
